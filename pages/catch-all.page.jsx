bf2993c1c4769fdfe361d512d1134c320884149c
Stephan Ringel <ringel@adobe.com>
2022-05-11 10:17:23 +0200

    Add Suspense and lazy for Separator component

--- pages/catch-all.page.jsx
+++ pages/catch-all.page.jsx
@@ -1,3 +1,4 @@
+import {Suspense, lazy} from 'react'
 import Teaser from "../components/Teaser";
 import strings from "./strings";
 import Container from "../components/Container";
@@ -9,14 +10,20 @@ export default function Page({model}) {
   if (model?.header?.path === '/content/demo-site/en/home') {
     const topTeaserProps = model.body.content.children[0].children[0].children[1].children[0].props
     const bottomTeaserProps = model.body.content.children[0].children[0].children[2].children[3].props
-
+  
+    const Separator = lazy(() => import("../components/Separator"));
+    // const Separator = lazy(() => import("https://franklin-project.pages.dev/components/Separator"));
+    
     return (
       <>
         <Teaser {...topTeaserProps} buttons={strings.teaserButtons} useAltStyles={false} />
         <Container>
           <Title>Home</Title>
           <p>{strings.contentText}</p>
-          <Separator />
+          <Suspense>
+            <Separator />
+          </Suspense>
+          
           <Flex direction="row" gap={20}>
             <Teaser {...bottomTeaserProps} buttons={strings.teaserButtons} />
             <Teaser {...topTeaserProps} buttons={strings.teaserButtons} />
